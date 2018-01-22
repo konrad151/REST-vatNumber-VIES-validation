eee0364b5f3bad55b7e29c68167b4166059efb3e
Konrad <konrad.k151@gmail.com>
2018-01-22 22:57:35 +0100

    init - working validation request by typing countryCode and vatNumber z obsługą even errora

---
+++ js/restValidation.js
@@ -0,0 +1,37 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+  var checkButton = document.getElementById('check-button');
+  checkButton.addEventListener("click", function(){
+    var countryCode;
+    var vatNumber;
+    var countryCodeValue = document.getElementById('country-code').value;
+    var vatNumberValue = document.getElementById('vat-number').value;
+
+    countryCode = countryCodeValue;
+    vatNumber = vatNumberValue;
+
+    var content = document.getElementsByClassName('response-content')[0];
+
+    var url  = "https://www.isvat.eu/" + countryCode + "/" + vatNumber;
+    var xhr  = new XMLHttpRequest()
+    xhr.open('GET', url, true)
+    xhr.addEventListener('load', function () {
+      var content = document.getElementsByClassName('response-content')[0];
+    	var data = JSON.parse(xhr.responseText);
+    	if (xhr.readyState == 4 && xhr.status == 200) {
+          if(data.valid == true){
+            content.innerHTML = "Valid: " + data.valid + "<br>Name: " + data.name[0] + "<br>Address: " + data.address[0];
+          }else{
+            content.innerHTML = "Valid: " + data.valid;
+          }
+    	}
+    });
+
+    xhr.addEventListener('error', function () {
+        content.innerHTML = "Nieprawidłowe dane";
+    });
+
+    xhr.send(null);
+
+  });
+});
